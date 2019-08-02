7cd4d51950b95d7df546ff91eda3371009c0d58d
unknown <tranducquylhp@gmail.com>
2019-08-02 23:28:44 +0700

    ok

---
+++ src/MyListTest.java
@@ -0,0 +1,28 @@
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> myList = new MyList<Integer>();
+
+        myList.add(1,1);
+        myList.add(1,2);
+        myList.add(1,3);
+        myList.add(1,4);
+
+        myList.remove(2);
+
+        for (int i=0; i<myList.size(); i++){
+            System.out.println(myList.get(i));
+        }
+
+        boolean contains = myList.contains(5);
+        System.out.println(contains);
+
+        System.out.println(myList.indexOf(3));
+
+        System.out.println(myList.add(2));
+        for (int i=0; i<myList.size(); i++){
+            System.out.println(myList.get(i));
+        }
+
+        myList.clear();
+    }
+}
